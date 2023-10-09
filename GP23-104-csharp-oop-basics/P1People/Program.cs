af972855aa581001b9cc3ae4d1ed10e391f3f860
mandel cohen <mandel.cohen@student.forsbergsskola.se>
2023-10-09 16:00:00 +0200

    Add second for loop

--- GP23-104-csharp-oop-basics/P1People/Program.cs
+++ GP23-104-csharp-oop-basics/P1People/Program.cs
@@ -6,6 +6,11 @@ Person[] people = new Person[3];
 
 for (var i = 0; i < people.Length; i++)
 {
+    people[i] = new Person();
+}
+
+for (var i = 0; i < people.Length; i++)
+{
     Console.WriteLine(people[i]);
 }
 
