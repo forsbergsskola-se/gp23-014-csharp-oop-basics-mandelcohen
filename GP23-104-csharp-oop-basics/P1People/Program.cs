32923b53c9a06bc82d417bf2fb5469ad369948a5
mandel cohen <mandel.cohen@student.forsbergsskola.se>
2023-10-09 15:06:35 +0200

    Updater P1_1Classes

--- GP23-104-csharp-oop-basics/P1People/Program.cs
+++ GP23-104-csharp-oop-basics/P1People/Program.cs
@@ -5,6 +5,9 @@ using P1People;
 Person person = new Person();
 
 
+
+
+
 Console.WriteLine(person);
 
 
--- GP23-104-csharp-oop-basics/P1_1Classes/Program.cs
+++ GP23-104-csharp-oop-basics/P1_1Classes/Program.cs
@@ -1,2 +1,11 @@
 ﻿// mandel
 
+using P1_1Classes;
+
+Person person = new Person();
+Animal animal = new Animal();
+Car car = new Car();
+
+Console.WriteLine(person);
+Console.WriteLine(animal);
+Console.WriteLine(car);
