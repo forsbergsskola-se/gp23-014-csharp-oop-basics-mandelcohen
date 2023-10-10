6eb6041e895b4fa7cc789ef2a3d7573e1cc481b6
mandel cohen <mandel.cohen@student.forsbergsskola.se>
2023-10-10 10:55:31 +0200

    Submit P2_1

--- GP23-104-csharp-oop-basics/P2_1PlayerGold/Program.cs
+++ GP23-104-csharp-oop-basics/P2_1PlayerGold/Program.cs
@@ -1,3 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
+Player player = new Player();
 
-Console.WriteLine("Hello, World!");
+player.gold = 200;
+
+Console.WriteLine(player.gold);
