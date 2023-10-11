02561d70d369fa4ca75745514e79e580fde2d05a
mandel cohen <mandel.cohen@student.forsbergsskola.se>
2023-10-11 09:38:09 +0200

    Submit P3_1

--- GP23-104-csharp-oop-basics/P3_1People/Person.cs
+++ GP23-104-csharp-oop-basics/P3_1People/Person.cs
@@ -4,4 +4,8 @@ public class Person
 {
     public string name;
     
+    public void Greeting(string name)
+    {
+        Console.WriteLine($"Hello, {name}");
+    }
 }
--- GP23-104-csharp-oop-basics/P3_1People/Program.cs
+++ GP23-104-csharp-oop-basics/P3_1People/Program.cs
@@ -1,3 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
+using P3_1People;
 
-Console.WriteLine("Hello, World!");
+Person[] persons = new Person[3];
+
+for (var i = 0; i < persons.Length; i++)
+{
+    persons[i] = new Person();
+}
+
+for (var i = 0; i < persons.Length; i++)
+{
+    Console.WriteLine("Give me a name:");
+    persons[i].name = Console.ReadLine();
+}
+
+for (var i = 0; i < persons.Length; i++)
+{
+    persons[i].Greeting(persons[i].name);
+}
